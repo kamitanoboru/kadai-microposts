0c91101399a424abb49ea96038466ec445c33add
Noboru.kamita <noboru.kamita@gamil.com>
2018-10-06 09:55:17 +0000

    user pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+                    
+                    <?php $now=date('Y/m/d H:i:s');?>
+                    {{ $now }}
+                
+    @if (Auth::check())
+        <?php $user = Auth::user(); ?>
+        {{ $user->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-                        <div style="text-align:center">
-                    @php
-                        $now=date('Y/m/d H:i:s');
-                        print $now;
-                    @endphp
-                </div>
-    </div>
+    @endif
 @endsection
